eb6befc14ec879f33520626cf10d6c3f732efd42
nikhil-kamatham <62095673+nikhil-kamatham@users.noreply.github.com>
2026-07-03 18:51:56 +0800

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-storage-demo"
+}
+
+variable "location" {
+  default = "Central US"
+}
+
+variable "storage_account_name" {
+  default = "tfstorageacctdemo1234"
+}
